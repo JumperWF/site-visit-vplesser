f26e03a53179a728b85a5d7e32cf79511ad402d0
Dmitrii <jumperwf@gmail.com>
2023-03-12 14:35:37 +0200

    v4

--- src/pages/components/Render.jsx
+++ src/pages/components/Render.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useRef } from "react";
 import './css/Render.css';
+import checkBtn from './img/check.png'
 
 const Render = (props) => {
     const aboutRef = useRef();
@@ -8,26 +9,34 @@ const Render = (props) => {
     const openTab = () => {
   
         let hiddenDiv = aboutRef.current.querySelector('.hiddenDiv');
-        let vert = aboutRef.current.querySelector('.vert');
-        let animSvg = aboutRef.current.querySelector('.animSvg');
+        // let vert = aboutRef.current.querySelector('.vert');
+        // let animSvg = aboutRef.current.querySelector('.animSvg');
+        let animCheck = aboutRef.current.querySelector('.checkImg');
+
         
         if (hiddenDiv.style.height === 'auto') {
-            hiddenDiv.style.height = '0';
             hiddenDiv.classList.remove('scrollDown');
-            animSvg.classList.remove('spinnerSvg');
+            hiddenDiv.style.height = '0';
+            // animSvg.classList.remove('spinnerSvg');
+            animCheck.classList.remove('spinnerImg');
+            animCheck.classList.remove('checkImg-rotate');
             
-            setTimeout(() => {
-                vert.classList.remove('hidden');// спрятать +, проявить -
-            }, 500);
+            // setTimeout(() => {
+            //     vert.classList.remove('hidden');// спрятать +, проявить -
+            // }, 500);
 
            
         } else {
-            hiddenDiv.style.height = 'auto';
             hiddenDiv.classList.add('scrollDown');
-            animSvg.classList.add('spinnerSvg');
-            setTimeout(() => {
-                vert.classList.add('hidden');// спрятать +, проявить -
-            }, 500);
+            hiddenDiv.style.height = 'auto';
+            // animSvg.classList.add('spinnerSvg');
+            animCheck.classList.add('spinnerImg');
+            animCheck.classList.add('checkImg-rotate');
+
+
+            // setTimeout(() => {
+            //     vert.classList.add('hidden');// спрятать +, проявить -
+            // }, 500);
 
             
         }
@@ -41,15 +50,17 @@ const Render = (props) => {
       <div className="Title" ref={aboutRef}>
             <h3>
                 <button onClick={openTab}>
-                <svg className = 'animSvg' aria-hidden="true" focusable="false" viewBox="0 0 10 10">
+                {/* <svg className = 'animSvg' aria-hidden="true" focusable="false" viewBox="0 0 10 10">
                     <rect className="vert" height="8" width="2" y="1" x="4"/>
                     <rect height="2" width="8" y="4" x="1"/>
-                </svg> 
+                </svg>  */}
+                <img src={checkBtn} alt="check" className='checkImg' />
                 {props.buttonText}
                 </button> 
             </h3>
             <div className="hiddenDiv">
                 {props.hiddenText}
+            <button onClick={openTab} className='closeThisDiv'>Свернуть</button>
             </div>
         </div>
     )
